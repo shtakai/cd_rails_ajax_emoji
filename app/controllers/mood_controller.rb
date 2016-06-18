8964df9d96a06e4818fbbe38e994cf49b8515fd3
Alyson Takai <shtakai@gmail.com>
2016-06-17 21:15:23 -0700

    implemented controller

--- app/controllers/mood_controller.rb
+++ app/controllers/mood_controller.rb
@@ -1,9 +1,14 @@
 class MoodController < ApplicationController
+  @@EMOJI_ULI = 'https://api.github.com/emojis'
   def index
   end
 
   def get_mood
-
+    code = params[:emoji] ? params[:emoji] : ''
+    agent = Mechanize.new
+    result = JSON.parse agent.get(@@EMOJI_ULI).body
+    @image_url = result[code.to_s]
+    flash[:alert] = "I'm sorry. I could not recognize your mood. Please try again." if @image_url.blank?
   end
 
 end
